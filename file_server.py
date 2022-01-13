8b9657bd345664aa048dfe00cf282e0aded45327
Armianto Sumitro <armiantos@gmail.com>
2022-01-12 20:15:29 -0700

    Combile file server handler code

--- file_server.py
+++ file_server.py
@@ -8,30 +8,45 @@ class FileServer:
         self.directory_path = directory_path
 
     def handle(self, request: Request) -> bool:
+        # Match route
         if not request.uri.startswith(self.base_path):
+            return False
+
+        # TODO: Test with flask to see if this is the desired behavior
+        if request.method != 'GET':
+            request.respond_with_raw(
+                message_body={},
+                content_type=CONTENT_TYPES['json'],
+                status_code=405
+            )
             return False
 
         requested_file_path = f'{self.directory_path}{request.uri}'
         if requested_file_path.endswith('/'):
             requested_file_path += 'index.html'
 
-        self.__send_http_response(request, requested_file_path)
-
-        return True
-
-    def __send_http_response(self, request: Request, file_path: str) -> str:
         # https://www.w3schools.com/python/python_file_open.asp
         try:
-            file = open(file_path, encoding='utf-8')
-            payload = file.read()
-            extension = file_path.split('.')[-1]
+            file = open(requested_file_path, encoding='utf-8')
 
-            request.respond_with_raw(
-                message_body=payload,
-                content_type=CONTENT_TYPES[extension],
-                status_code=200
-            )
         except FileNotFoundError as err:
-            return request.respond_with_json({'err': err.strerror}, status_code=404)
-        else:
-            return request.respond_with_json({'err': 'Unknown error occured'}, status_code=500)
+            request.respond_with_json({'err': err.strerror}, status_code=404)
+            return False
+        except:
+            request.respond_with_json(
+                {'err': 'Unknown error occured'}, status_code=500)
+            return False
+
+        payload = file.read()
+        extension = requested_file_path.split('.')[-1]
+        print(f'{extension}: {CONTENT_TYPES[extension]}')
+
+        request.respond_with_raw(
+            message_body=payload,
+            content_type=CONTENT_TYPES[extension],
+            status_code=200
+        )
+
+        file.close()
+
+        return True
